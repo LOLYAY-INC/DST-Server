a72eee6d60239a759890fe39ea8d5ca160ee3d39
LOLYAY <github@lolyay.de>
2025-11-11 23:15:20 +0100

    Add TODO for interface conversion for plugin API
    Added TODO comment to indicate future changes for plugin API.

--- server/src/main/java/io/lolyay/discordmsend/server/music/consumers/ITrackConsumer.java
+++ server/src/main/java/io/lolyay/discordmsend/server/music/consumers/ITrackConsumer.java
@@ -5,6 +5,6 @@ import io.lolyay.discordmsend.server.music.providers.HighQualityOpusStreamer;
 public interface ITrackConsumer {
     void stop();
     void start();
-
+    //TODO make this into an interface in preperation to plugin api
     HighQualityOpusStreamer getStreamer();
 }
